afd9d72d5264f11cab8f05c9f73bea375d46b573
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-11 10:10:10 -0600

    finished part 3

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,13 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require("./utils");
 
-describe('shuffleArray should', () => {
-    // CODE HERE
-})
+test("length", () => {
+  const shuffled = shuffleArray([1, 3, 4, 5, 6]);
+  expect(shuffled).toHaveLength(5);
+});
+
+test("contains", () => {
+  const expected = [5, 4, 3, 2, 1];
+  expect(shuffleArray([1, 2, 3, 4, 5])).toEqual(
+    expect.arrayContaining(expected)
+  );
+});
