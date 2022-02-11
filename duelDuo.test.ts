bd19f12cacf323f2e0c718c78bbbfbe3986b2f19
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-11 11:05:08 -0600

    finished part 4

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,20 +1,43 @@
+import { Builder, Capabilities, By } from "selenium-webdriver";
 
-import { Builder, Capabilities, By } from "selenium-webdriver"
+require("chromedriver");
 
-require('chromedriver')
-
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
-})
+  driver.get("http://localhost:3000/");
+});
 
 afterAll(async () => {
-    driver.quit()
-})
+  driver.quit();
+});
 
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-})
+test("Title shows up when page loads", async () => {
+  const title = await driver.findElement(By.id("title"));
+  const displayed = await title.isDisplayed();
+  expect(displayed).toBe(true);
+
+  await driver.sleep(3000);
+});
+
+test("Draw button -> choices", async () => {
+  const drawButton = await driver.findElement(By.id("draw"));
+  await drawButton.click();
+  const divs = await driver.findElement(By.id("choices"));
+  const displaying = await divs.isDisplayed();
+  expect(displaying).toBe(true);
+
+  await driver.sleep(5000);
+});
+
+test("adding a bot with add to duo button", async () => {
+  const drawButton = await driver.findElement(By.id("draw"));
+  await drawButton.click();
+  const botBtn = await driver.findElement(By.className("bot-btn"));
+  await botBtn.click();
+  const playerBotHeader = await driver.findElement(By.id("your-duo-header"));
+  const isDisplayed = await playerBotHeader.isDisplayed();
+
+  expect(isDisplayed).toBe(true);
+  await driver.sleep(5000);
+});
